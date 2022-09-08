e01c0bf4af3c4719bd22b301ebdd27e0e1a43a56
Konrad Schmid <dev.k@skschmid.de>
2022-09-08 15:35:07 +0200

    Add re-connecting for mqtt

--- src/KMqtt.cpp
+++ src/KMqtt.cpp
@@ -1,14 +1,20 @@
 #include "KMqtt.h"
 
+#define TIME_TO_BE_CONNECTED_100MS 20
+
 KMqtt::KMqtt()
 {
+    status = KMQTT_NOT_CONNECTED;
 }
 
 KMqtt::~KMqtt()
 {
 }
-void KMqtt::setup(WiFiClient &espClient, String &mqttServer, const uint16_t port, String &id)
+void KMqtt::setup(KSchedule *kscheduler, WiFiClient &espClient, String &mqttServer, const uint16_t port, String &id)
 {
+    this->kscheduler = kscheduler;
+    this->id = id;
+
     pubsubclient.setClient(espClient);
     pubsubclient.setServer(mqttServer.c_str(), port);
     while (!pubsubclient.connected())
@@ -18,6 +24,10 @@ void KMqtt::setup(WiFiClient &espClient, String &mqttServer, const uint16_t port
     }
 
     pubsubclient.setCallback(std::bind(&KMqtt::mqttCallback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+
+    status = KMQTT_CONNECTED;
+
+    kmqtt_100ms();
 }
 void KMqtt::loop()
 {
@@ -58,4 +68,72 @@ String KMqtt::payloadToString(byte *payload, unsigned int length)
     memcpy(messageBuffer, payload, length); // copy in the payload
     messageBuffer[length] = '\0';
     return String(messageBuffer);
+}
+
+void KMqtt::kmqtt_100ms()
+{
+    if (!pubsubclient.connected())
+    {
+        switch (status)
+        {
+        case KMQTT_CONNECTED:
+
+            // It was connected, but connection get's lost
+
+            status = KMQTT_WAITING_NETWORK;
+            // no break;
+
+        case KMQTT_WAITING_NETWORK:
+
+            if (WiFi.status() != WL_CONNECTED)
+            {
+                // No Wifi, wait till WiFi is available, then go on
+            }
+            else
+            {
+                // WiFi available, do mqtt connecting
+                timeoutCtr = TIME_TO_BE_CONNECTED_100MS;
+                pubsubclient.connect(id.c_str());
+            }
+
+            status = KMQTT_CONNECTING;
+
+            break;
+
+        case KMQTT_CONNECTING:
+            // Still connecting, just waiting
+            if (timeoutCtr > 0)
+            {
+                timeoutCtr--;
+            }
+            else
+            {
+                // Timeout reached, do restart
+                status = KMQTT_RESTART;
+            }
+
+            break;
+
+        case KMQTT_RESTART:
+            // Waiting time ended. Restart ESP
+            ESP.restart();
+            break;
+
+        default:
+            // assume we are connected
+            break;
+        }
+    }
+    else
+    {
+        if (status == KMQTT_CONNECTING)
+        {
+            // Status was connecting, but now successful connected
+            pubsubclient.setCallback(std::bind(&KMqtt::mqttCallback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        }
+
+        status = KMQTT_CONNECTED;
+    }
+
+    kscheduler->schedule(std::bind(&KMqtt::kmqtt_100ms, this), 100);
 }
